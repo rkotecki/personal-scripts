60866e81fb012b568d8911ac430927467ef10253
Ryan Kotecki <rkotecki@ryans-imac.attlocal.net>
2018-11-26 09:39:34 -0600

    Added numbers function

--- chefStats/chefStats.sh
+++ chefStats/chefStats.sh
@@ -15,6 +15,7 @@
 VERSION="0.1.1"
 KNIFE_DIR="/root/chef-repo/.chef"
 CHEF_DIR="/opt/chef"
+SUPERMARKET='https://supermarket.chef.io'
 
 ################################################################################
 # Set help menu for users to understand script and flags
@@ -33,4 +34,23 @@ helpMenu()
   echo "  -h, --help              Display this menu and exit"
   echo " "
   echo "Report chefStats bugs to https://github.com/rkotecki/personal-scripts/issues"
+}
+
+################################################################################
+# Set numbers function to pull numbers from servers
+################################################################################
+numbers()
+{
+  PROD_CBOOK_NUM=`knife cookbook list -c $KNIFE_DIR/knife.rb|wc -l`
+  PROD_NODE_NUM=`knife node list -c $KNIFE_DIR/knife.rb|wc -l`
+  DEV_CBOOK_NUM=`knife cookbook list -c $KNIFE_DIR/knife_dev.rb|wc -l`
+  DEV_NODE_NUM=`knife coobkook list -c $KNIFE_DIR/knife_dev.rb|wc -l`
+  SPRMRT_NUM=`knife cookbook site list -m $SUPERMARKET -w -c $KNIFE_DIR/knife.rb|wc -l`
+
+  echo " "
+  echo "Number of cookbooks on DEVELOPMENT Chef server:            $DEV_CBOOK_NUM"
+  echo "Number of cookbooks on PRODUCTION Chef server:             $PROD_CBOOK_NUM"
+  echo "Number of cookbook on Chef Supermarket:                    $SPRMRT_NUM"
+  echo "Number of nodes registered to DEVELOPMENT Chef server:     $DEV_NODE_NUM"
+  echo "Number of nodes registered to PRODUCTION Chef server:      $PROD_NODE_NUM"
 }
